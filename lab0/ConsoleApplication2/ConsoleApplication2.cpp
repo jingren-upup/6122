c8fcbe3eb454e783db82ee2fc554926d268d84b9
jingren-upup <60909784+jingren-upup@users.noreply.github.com>
2020-11-26 16:44:06 +0800

    Add files via upload

---
+++ lab0/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,72 @@
+﻿// Remember your file headers and comments
+
+
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include "MyGrid.h"
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    fstream outFile("Lab2Output.txt", fstream::out);
+
+    vector<MyGrid> vecGrids;
+    MyGrid tmpGrid;
+
+    vecGrids.resize(argc);
+    for (int ii = 1; ii < argc; ii++)
+    {
+        if (!vecGrids[ii - 1].loadGridFromFile(argv[ii]))
+        {
+            outFile << "Failed to load file: " << argv[ii] << endl;
+        }
+    }
+
+    if (argc >= 3)
+    {
+        //        tmpGrid += vecGrids[1];
+        //        outFile << tmpGrid << endl;
+
+
+        //        tmpGrid = vecGrids[0] + vecGrids[1];
+        //        outFile << tmpGrid << endl;
+        //
+        //        tmpGrid = vecGrids[0] - vecGrids[1];
+        //        outFile << tmpGrid << endl;
+        //
+        //        tmpGrid = vecGrids[0] + 10;
+        //        outFile << tmpGrid << endl;
+        //
+        //        tmpGrid = 10 + vecGrids[1] + 10;
+        //        outFile << tmpGrid << endl;
+        //
+        //        outFile << tmpGrid++ << endl;
+        //        outFile << ++tmpGrid << endl;
+        //
+        //        outFile << tmpGrid-- << endl;
+        //        outFile << --tmpGrid << endl;
+        //
+        //
+        //        outFile << -vecGrids[0] << endl;
+        //
+        //        if (vecGrids[0] == vecGrids[1])
+        //        {
+        //            outFile << "The first two grids are equal" << endl;
+        //        }
+        //        else
+        //        {
+        //            outFile << "The first two grids are not equal" << endl;
+        //        }
+        //
+        //        outFile << "The first grid has " << vecGrids[0].getNumRows() << " rows and " << vecGrids[0].getNumCols() << " cols." << endl;
+        //
+        //        vecGrids[0].zeroOutGrid();
+        //        outFile << vecGrids[0] << endl;
+
+    }
+
+    outFile.close();
+
+}
